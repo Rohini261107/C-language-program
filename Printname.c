894acf33dfd5ca04f6f18babd0caf55a30e224b7
Rohini261107 <rohinitodgire07@gmail.com>
2025-11-08 11:20:56 +0530

    Add files via upload

---
+++ Printname.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    char name[6];
+    int i;
+
+    printf("Enter your name :");
+    scanf("%c",&name[i]);
+    for(i=0;i<6;i++)
+    {
+     printf("%c\n",name[i]);   
+    }
+    
+
+
+return 0;
+
+}
