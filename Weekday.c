a95c6f0a562340f488f141d022f1c790eebf4c96
Rohini261107 <rohinitodgire07@gmail.com>
2025-11-08 11:35:44 +0530

    Add files via upload

---
+++ Weekday.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main()
+{
+    int week;
+    
+    printf("Enter a week number(1-7):");
+    scanf("%d",&week);
+
+    switch(week)
+    {
+        case 1:
+        printf("Monday \n");
+        break;
+
+        case 2:
+        printf("Tuesday \n");
+        break;
+
+        case 3:
+        printf("Wednesday \n");
+        break;
+
+        case 4:
+        printf("Thursdau \n");
+        break;
+
+        case 5:
+        printf("Friday \n");
+        break;
+
+        case 6:
+        printf("Saturday \n");
+        break;
+    
+        case 7:
+        printf("Sunday \n");
+        break;
+
+        default:
+        printf("Invalid input! please enter a week number between 1 and 7 \n");
+    }
+    
+
+    
+    return 0;
+}
