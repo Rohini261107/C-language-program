e6a8f03c8ce82c88352f79a44435721c64c3accb
Rohini261107 <rohinitodgire07@gmail.com>
2025-11-08 11:33:49 +0530

    Add files via upload

---
+++ Triangletype.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c;
+
+    printf("Enter three sides of triangle: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("Equilateral triangle\n");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles triangle\n");
+    else
+        printf("Scalene triangle\n");
+
+    return 0;
+}
