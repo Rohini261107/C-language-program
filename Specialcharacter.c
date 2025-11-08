b561b7a1fdaf086c1704512bc55690d26075017e
Rohini261107 <rohinitodgire07@gmail.com>
2025-11-08 11:26:11 +0530

    Add files via upload

---
+++ Specialcharacter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("It is an alphabet.\n");
+    else if (ch >= '0' && ch <= '9')
+        printf("It is a digit.\n");
+    else
+        printf("It is a special character.\n");
+
+    return 0;
+}
