2b9dcc9f2fb5f794fd4b13a4c667169e2c55f8b4
Rohini261107 <rohinitodgire07@gmail.com>
2025-11-08 11:15:52 +0530

    Add files via upload

---
+++ Check _numberp+ve ,n-ve ,zero.c
@@ -0,0 +1,21 @@
+#include<conio.h>
+int main()
+{
+    int number;
+    printf("Enter a number:");
+    scanf("%d",&number);
+
+    if(number>0)
+    {
+        printf("The number is positive");
+    }
+    else if ( number<0)
+    {
+        printf("The number is negative");
+    }
+    else{
+        printf("The number is zero");
+    }
+    
+    return 0;
+}
